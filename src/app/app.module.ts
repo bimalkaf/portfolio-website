8a4a6b32e7f6e5666f7ebef7cc4a195b141bf46a
bimalkaf <bimalisnotbimal@gmail.com>
2023-03-08 23:35:51 +0530

    added responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { ResponsiveDirective } from './directives/responsive.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerComponent
+    BannerComponent,
+    ResponsiveDirective
   ],
   imports: [
     BrowserModule
