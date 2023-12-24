e98c9d8ba1dc3b1cc311a8c8af4af542b9330361
Bimal kafle <bimalkafle@bimals-MacBook-Pro.local>
2023-12-24 10:24:21 +0545

    dark mode

--- docs/script.js
+++ docs/script.js
@@ -8,3 +8,20 @@ function toggleMenu() {
 function openNewTab(link) {
   window.open(link, '_blank');
 }
+
+function toggleDarkMode() {
+  const body = document.body;
+  body.classList.toggle("dark-mode");
+
+  // You can also store the user's preference in localStorage for persistence
+  const isDarkMode = body.classList.contains("dark-mode");
+  localStorage.setItem("darkMode", isDarkMode);
+}
+
+// Check for user's preference on page load
+document.addEventListener("DOMContentLoaded", () => {
+  const isDarkMode = localStorage.getItem("darkMode") === "true";
+  if (isDarkMode) {
+    document.body.classList.add("dark-mode");
+  }
+});
